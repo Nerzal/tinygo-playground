67741cc0b297d2b47b2d76d4d7611d0bf17876a2
Tobias Theel <tt@fino.digital>
2020-02-16 20:36:11 +0100

    implemented traffic lights!

--- cmd/traffic-light/light.go
+++ cmd/traffic-light/light.go
@@ -20,6 +20,32 @@ func main() {
 
 	testLEDs(redLED, yellowLED, greenLED)
 
+	// Traffic-Lights
+	// RED
+	// RED-YELLOW
+	// YELLOW
+	// GREEN
+	// YELLOW
+	// RED
+
+	for {
+		redLED.High()
+		sleep(1000)
+		yellowLED.High()
+		time.Sleep(500)
+		redLED.Low()
+		time.Sleep(1000)
+		yellowLED.Low()
+		greenLED.High()
+		time.Sleep(1000)
+		greenLED.Low()
+		yellowLED.High()
+		time.Sleep(1000)
+	}
+}
+
+func sleep(duration time.Duration) {
+	time.Sleep(time.Millisecond * 1000)
 }
 
 func testLEDs(redLED, yellowLED, greenLED machine.Pin) {
